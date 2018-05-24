60ced1e70458cbb7338081ca8c73c019831447c8
LXG <lxg1986@gmail.com>
2018-05-24 10:22:08 +0800

    init project

---
+++ src/assets/js/app.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+import './lib//jQuery.countdown.min';
+
+window.$ = $;
+
+import Foundation from 'foundation-sites';
+import line from './line';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+$(document).foundation();
+
+line();
+
+$("#time-start")
+  .countdown("2018/06/15", function(event) {
+    $(this).text(
+      event.strftime('%D days %H:%M:%S')
+    );
+  });
